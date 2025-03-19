50c37cf788a82d0aa53ec4b3c15db94291b1373a
tanaya <ttanyasharma123@gmail.com>
2025-03-19 15:18:13 +0530

    [Tanya]:Add UC5 Implemented

--- Employee.js
+++ Employee.js
@@ -51,4 +51,19 @@ for (let day =0; day<NUM_OF_WORKING_DAYS; day++){
 }
  empWage = empHrs * WAGE_PER_HOUR;
  console.log("Total Hrs: "+empHrs+ "Emp Wage: " +empWage);
+ //UC5
+ const MAX_HRS_IN_MONTH = 100;
+   const NUM_OF_WORKING_DAY = 10;
+  totalEmpHrs = 0;
+  let totalWorkingDays = 0;
+  while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAY){
+    totalWorkingDays++;
+    empCheck = Math.floor(Math.random()*10) %3;
+    totalEmpHrs += getWorkingHours(empCheck);
+  }
+   empWage = totalEmpHrs * WAGE_PER_HOUR;
+   console.log("UC5- Total Days: " + totalWorkingDays + "Total Hrs:" + totalEmpHrs + "Emp Wage: " + empWage);
+
+
+
 
