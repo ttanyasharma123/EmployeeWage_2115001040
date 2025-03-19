461509f5fb111f33b86ef95f08d7348479047d96
tanaya <ttanyasharma123@gmail.com>
2025-03-19 14:48:37 +0530

    [Tanya]:Add UC3 Implemented

--- Employee.js
+++ Employee.js
@@ -13,7 +13,7 @@ const  PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-let empHrs = 0;
+  let empHrs = 0;
 empCheck = Math.floor(Math.random()* 10) %3;
 switch (empCheck){
     case IS_PART_TIME:
@@ -25,5 +25,20 @@ switch (empCheck){
     default :
         empHrs = 0;
     }
-let empWage = empHrs * WAGE_PER_HOUR;
+  let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Emp Wage:" + empWage); 
+function getWorkingHours(empCheck){
+    switch (empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+            case IS_FULL_TIME:
+                return FULL_TIME_HOURS;
+                default:
+                    return 0;
+    }
+}
+ empHrs = 0;
+ empCheck = Math.floor(Math.random()*10) % 3;
+empHrs = getWorkingHours(empCheck);
+ empWage = empHrs * WAGE_PER_HOUR;
+console.log ("Emp Wage: "+ empWage);
