1e2d694c43319a6255bb08eca36cfdc2c0715e58
tanaya <ttanyasharma123@gmail.com>
2025-03-19 15:04:04 +0530

    [Tanya]:Add UC4 Implemented

--- Employee.js
+++ Employee.js
@@ -40,5 +40,15 @@ function getWorkingHours(empCheck){
  empHrs = 0;
  empCheck = Math.floor(Math.random()*10) % 3;
 empHrs = getWorkingHours(empCheck);
- empWage = empHrs * WAGE_PER_HOUR;
+empWage = empHrs * WAGE_PER_HOUR;
+  
 console.log ("Emp Wage: "+ empWage);
+const NUM_OF_WORKING_DAYS = 2;
+empHrs = 0;
+for (let day =0; day<NUM_OF_WORKING_DAYS; day++){
+    let empCheck = Math.floor(Math.random()*10) %3;
+    empHrs += getWorkingHours(empCheck);
+}
+ empWage = empHrs * WAGE_PER_HOUR;
+ console.log("Total Hrs: "+empHrs+ "Emp Wage: " +empWage);
+
