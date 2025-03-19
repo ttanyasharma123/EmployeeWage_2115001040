0a3b3558ca49faa4a1adb55740ff88705cb4a791
tanaya <ttanyasharma123@gmail.com>
2025-03-19 14:28:40 +0530

    [Tanya]:Add UC2 implemented

--- Employee.js
+++ Employee.js
@@ -7,3 +7,23 @@ if (empCheck == IS_ABSENT){
 }else{
     console.log("Employee is PRESENT");
 }
+const IS_PART_TIME = 1;
+const IS_FULL_TIME =2;
+const  PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+let empHrs = 0;
+empCheck = Math.floor(Math.random()* 10) %3;
+switch (empCheck){
+    case IS_PART_TIME:
+        empHrs = PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+        empHrs = FULL_TIME_HOURS;
+        break;
+    default :
+        empHrs = 0;
+    }
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Emp Wage:" + empWage); 
