7f03a2d07a1091ba69803df42ade646aeb8e5167
tanaya <ttanyasharma123@gmail.com>
2025-03-19 17:10:03 +0530

    [Tanya]:Add UC7 Implemented

--- Employee.js
+++ Employee.js
@@ -82,6 +82,69 @@ for (let day =0; day<NUM_OF_WORKING_DAYS; day++){
      }
      empWage = calcDailyWage(totalEmpHrs);
      console.log("UC6 - Total Days :" + totalWorkingDays + "Total Hrs:" + totalEmpHrs + "Emp Wage:" + empWage);
+     //UC7
+     let totEmpWage = 0;
+     function sum(dailyWage){
+        totEmpWage += dailyWage;
+
+     }
+
+     empDailyWageArr.forEach(sum);
+     console.log("UC7A-Total Days:" + totalWorkingDays+" Total Hrs: " + totalEmpHrs + "Emp Wage:" + totEmpWage);
+     function totalWages(totalWage, dailyWage){
+        return totalWage + dailyWage;
+     }
+     console.log("UC7A- Emp Wage with reduce:" + empDailyWageArr.reduce(totalWages,0));
+     //UC7 -B
+     let dailyCntr = 0;
+     function mapDayWithWage(dailyWage){
+        dailyCntr++;
+        return dailyCntr + "=" + dailyWage;
+     }
+     let mapDayWithWageArr = empDailyWageArr.map(mapDayWithWage);
+     console.log("UC7B-Daily Wage Map");
+
+     console.log(mapDayWithWageArr);
+
+     //UC7-C
+
+     function fulltimeWage(dailyWage){
+        return dailyWage.includes("160");
+
+     }
+     let fullDayWageArr = mapDayWithWageArr.filter(fulltimeWage);
+     console.log("UC7C - Daily Wage Filter When Fulltime Wage Earned");
+     console.log(fullDayWageArr);
+
+     //UC7-D
+     function findFulltimeWage(dailyWage){
+        return dailyWage.includes("160");
+
+     }
+     console.log("UC 7D- First time Fulltime wage was earned on Day: "+mapDayWithWageArr.find(findFulltimeWage));
+
+     //UC7- E 
+     function isAllFulltimeWage(dailyWage){
+        return dailyWage.includes("160");
+     }
+     console.log("UC 6E - check All Elements have Full Time Wage: " + fullDayWageArr.every(isAllFulltimeWage));
+
+     //UC7-F
+     function isAnyPartTimeWage(dailywage){
+        return dailywage.includes("80");
+     }
+     console.log ("UC 7F - Check If Any Part Time Wage: "+ mapDayWithWageArr.some(isAnyPartTimeWage));
+
+     //UG 7G 
+
+     function totalDaysWorked(numOfDays, dailyWage){
+        if(dailyWage > 0 ) return numOfDays+1;
+        return numOfDays;
+
+     }
+     console.log ("UC 7G - Number of Days Emp Worked :"+ empDailyWageArr.reduce(totalDaysWorked,0));
+
+
 
 
 
