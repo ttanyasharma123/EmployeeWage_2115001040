d17001bc2590b6fbfdeffd78551d356488018e1c
tanaya <ttanyasharma123@gmail.com>
2025-03-20 15:45:32 +0530

    [Tanya]:Add UC10 Implemented

--- Employee.js
+++ Employee.js
@@ -54,7 +54,7 @@ for (let day =0; day<NUM_OF_WORKING_DAYS; day++){
  //UC5
  const MAX_HRS_IN_MONTH = 100;
    const NUM_OF_WORKING_DAY = 10;
-  totalEmpHrs = 0;
+  let totalEmpHrs = 0;
   let totalWorkingDays = 0;
   while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAY){
     totalWorkingDays++;
@@ -196,6 +196,28 @@ for (let day =0; day<NUM_OF_WORKING_DAYS; day++){
      console.log("Full Working Days: "+fullWorkingDays);
      console.log("Part Working Days: "+partWorkingDays);
      console.log("Non Working Days:" +nonWorkingDays);
+     //UC 10
+     totalEmpHrs =0;
+      totalWorkingDays = 0;
+      let empDailyHrsAndWageArr = new Array();
+      while (totalEmpHrs <= _HRS_IN_MONTHS && totalWorkingDays <NUM_OF_WORKING_DAYYS){
+         totalWorkingDays++;
+         let empCheck = Math.floor(Math.random() * 10)%3;
+         let empHrs = getWorkingHours(empCheck);
+         totalEmpHrs += empHrs;
+         empDailyHrsAndWageArr.push(
+            {
+               dayNum:totalWorkingDays,
+               dailyHours:empHrs,
+               dailyWage: calcDailyWage(empHrs),
+               toString(){
+                  return '\nDay'+ this.dayNum + '=> Working Hours is' + this.dailyHours +'And Wage Earned = ' + this.dailyWage
+               },
+
+            });
+      }
+      console.log("UC 10 Showing Daily Hours Worked and Wage Earned: "+empDailyHrsAndWageArr);
+
 
 
 
